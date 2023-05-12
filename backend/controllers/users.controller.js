6b92be838eba0058a5a5e392a504093e0941d55c
jvera <jvera@sentra.cl>
2023-05-12 18:52:43 -0400

    Login & encriptación / Creación de usuario

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
 const { pool } = require('../db/db');
+const saltRounds = 10;
+
 
 exports.getUser = async (req, res) => {
   try {
@@ -19,40 +22,53 @@ exports.getUser = async (req, res) => {
   }
 };
 
-exports.postCreatUser = async (req, res) => {
+exports.createUser = async (req, res) => {
   try {
+    const newUser = req.body;
+    console.log(newUser);
 
-      //funcionalidades crear usuario
+    if (!newUser.correo || !newUser.contrasena) {
+      throw new Error("Ingrese su correo y contraseña");
+    }
+    //Encontrar la ID mayor y sumar 1 para crear un nuevo usuario.
+    
+    const hashedPassword = await bcrypt.hash(newUser.contrasena, saltRounds);
+
+    const query = 'INSERT INTO usuario (id, correo, contrasena) VALUES ($1, $2, $3)';
+    const values = [newUser.id, newUser.correo, hashedPassword];
+    console.log('values:', values)
+
+    await pool.query(query, values);
+    console.log(`Usuario creado con éxito`);
 
     return res.status(200).send({
       success: true,
-      message: "usuario encontrado",
-      Data: result.rows
-    })
-
+      message: "usuario creado"
+    });
   } catch (error) {
-    console.error('Error al obtener los usuarios', error);
+    console.error('Error al crear usuario', error);
     res.status(500).send('Error en el servidor');
   }
 };
 
-async function login(correo, contraseña) {
+
+async function login(correo, contrasena) {    //add "sac." a usuario para BD cloud
   const query = {
-    loginQuery: `SELECT   correo, contraseña 
-                   FROM   sac.usuario 
-                   WHERE  correo = $1 
-                   AND    contraseña = $2`,
-    values: [correo, contraseña],
+    text: `SELECT correo, contrasena 
+           FROM usuario   
+           WHERE correo = $1 
+           AND contrasena = $2`,
+    values: [correo, contrasena],
   };
 
   const { rows } = await pool.query(query);
 
   if (rows.length === 0) {
-    return { success: false, message: 'Correo o contraseña incorrectos' };
+    return { success: false };
   }
-  return { success: true, message: 'Inicio de sesión exitoso' };
-}
 
+  return { success: true };
+}
 
 exports.loginUser = async (req, res) => {
   try {
@@ -62,11 +78,18 @@ exports.loginUser = async (req, res) => {
     }
     const resultado = await login(correo, contrasena);
 
-    return res.status(200).send({
-      success: true,
-      message: "Inicio de sesión exitoso",
-      Data: resultado
-    })
+    if (resultado.success) {
+      return res.status(200).send({
+        success: true,
+        message: "Inicio de sesión exitoso",
+        Data: resultado
+      })
+    } else {
+      return res.status(401).send({
+        success: false,
+        message: "Correo o contraseña incorrectos"
+      })
+    }
 
   } catch (error) {
     console.error('Error al comprobar las credenciales', error);
--- backend/db/db.js
+++ backend/db/db.js
@@ -4,13 +4,24 @@
 
 const { Pool } = require('pg');
 
-const pool = new Pool({
+/*const pool = new Pool({
   user: 'postgres',
   host: '34.176.7.179',
   database: 'portafolio',
   password: 'Ignacia77##',
   port: 5432,
+});*/
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'sac_local',
+  password: '0000001',
+  port: 5432,
 });
+
+
+
 
 module.exports = {
   pool,
